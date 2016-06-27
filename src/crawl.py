c0d7cac30d36603eb7019ae4fa04ff1961bbc2df
kriss <kriss.pujats@finabay.com>
2016-06-27 22:37:57 +0300

    cm

--- src/crawl.py
+++ src/crawl.py
@@ -4,10 +4,21 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(3, GPIO.OUT)
-GPIO.setup(4, GPIO.IN)
+GPIO.setup(5, GPIO.IN)
 
 while True:
 	GPIO.output(3, True)
-	time.sleep(1)
+	time.sleep(1/200000)
 	GPIO.output(3, False)
+	
+	while not GPIO.input(5):
+		pass
+	start = time.time()
+
+	while GPIO.input(5):
+		pass
+	delta = time.time() - start
+	distance = delta * 17150
+	
+	print(str(distance) + " cm")
 	time.sleep(1)
