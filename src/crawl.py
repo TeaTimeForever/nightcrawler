b550f1d475127e85f0678ba0fcf74fffe1d3d9a6
kriss <kriss.pujats@finabay.com>
2016-06-27 21:10:09 +0300

    initial

---
+++ src/crawl.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(4, GPIO.IN)
+
+while True:
+	GPIO.output(3, True)
+	time.sleep(1)
+	GPIO.output(3, False)
+	time.sleep(1)
