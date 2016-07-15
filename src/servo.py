2433b0098119ee003abfa6b3bf05a4c2d3228aee
kriss <kriss.pujats@finabay.com>
2016-07-15 23:40:16 +0300

    round sense

--- src/servo.py
+++ src/servo.py
@@ -1,10 +1,15 @@
 import RPi.GPIO as GPIO
 import time
 
-SERVO = 7
+SERVO_PIN = 7
+DISTANCE_IN_PIN = 11
+DISTANCE_OUT_PIN = 12
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(SERVO, GPIO.OUT)
-p = GPIO.PWM(SERVO, 50)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+GPIO.setup(DISTANCE_OUT_PIN, GPIO.OUT)
+GPIO.setup(DISTANCE_IN_PIN, GPIO.IN)
+p = GPIO.PWM(SERVO_PIN, 50)
 p.start(2.5)
 
 def turn(angle):
@@ -12,14 +17,28 @@ def turn(angle):
 	# print "timeout: " + str(dutyCycle)
 	p.ChangeDutyCycle(dutyCycle)
 
+def distance():
+	GPIO.output(DISTANCE_OUT_PIN, True)
+	time.sleep(0.000002)
+	GPIO.output(DISTANCE_OUT_PIN, False)
+
+	while not GPIO.input(DISTANCE_IN_PIN):
+		pass
+	start = time.time()
+
+	while GPIO.input(DISTANCE_IN_PIN):
+		pass
+	return (time.time() - start) * 17150
+
 try:
 	while True:
 		angle = 0
 		while angle<=180:
 			turn(angle)
-			print angle
 			angle += 15
-			time.sleep(1)
+			distance = distance()
+			print("angle = "+str(angle)+"; distance="+str(distance) + " cm")
+			time.sleep(0.1)
 except KeyboardInterrupt:
 	p.stop()
 	GPIO.cleanup()
