4b9bf4c54e4dc8a8a4ea7ec0bfb068ce4e1a7a72
Karina Pilyushonoka <k.pilyushonoka@gmail.com>
2016-07-22 01:25:22 +0300

    obstacles generator

--- src/canvas/canvas.js
+++ src/canvas/canvas.js
@@ -1,15 +1,35 @@
 /**
  * Created by eq on 22/07/16.
  */
-var example = document.getElementById("canvas"),
-  ctx     = example.getContext('2d');
-example.width  = 640;
-example.height = 480;
-ctx.strokeRect(15, 15, 266, 266);
-ctx.strokeRect(18, 18, 260, 260);
-ctx.fillRect(20, 20, 256, 256);
-for (i = 0; i < 8; i += 2)
-  for (j = 0; j < 8; j += 2) {
-    ctx.clearRect(20 + i * 32, 20 + j * 32, 32, 32);
-    ctx.clearRect(20 + (i + 1) * 32, 20 + (j + 1) * 32, 32, 32);
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = 600,
+      CANVAS_LENGTH = 600,
+      MIN_OBSTACLE_SIZE = 10,
+      MAX_OBSTACLE_SIZE = 30,
+      OBSTACLES_COUNT = 10;
+
+canvas.width  = CANVAS_WIDTH;
+canvas.height = CANVAS_LENGTH;
+
+generatObstacles(OBSTACLES_COUNT);
+
+function generatObstacles(count){
+  initWalls();
+  for(let i=0; i < count; i++) {
+    ctx.fillRect(
+      randomInRange(0, CANVAS_WIDTH-MAX_OBSTACLE_SIZE),
+      randomInRange(0, CANVAS_LENGTH-MAX_OBSTACLE_SIZE),
+      randomInRange(MIN_OBSTACLE_SIZE, MAX_OBSTACLE_SIZE),
+      randomInRange(MIN_OBSTACLE_SIZE, MAX_OBSTACLE_SIZE));
   }
+};
+
+function initWalls(){
+  ctx.strokeRect(0, 0, CANVAS_WIDTH, CANVAS_LENGTH);
+}
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
