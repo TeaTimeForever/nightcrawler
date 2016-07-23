c7fe00f8548c746c4b1a3068a4dd923fa82ffa88
karina.pilusonoka <karina.pilusonoka@twino.eu>
2016-07-23 03:55:51 +0300

    ideal scanner

--- src/canvas/physics/ideal.sensor.js
+++ src/canvas/physics/ideal.sensor.js
@@ -1,4 +1,5 @@
 import { Body } from "./body";
+import { cos } from "../utils/complex";
 
 export class IdealSensor extends Body {
 
@@ -7,6 +8,24 @@ export class IdealSensor extends Body {
     var relativeP1 = this.relative(p1),
         relativeP2 = this.relative(p2);
 
+    if(relativeP1.y * relativeP2.y > 0) {
+      return null;
+    } else {
+      let diff = minus(relativeP1, relativeP2);
+      if(diff.y == 0) {
+        return null;
+      }
+      let x = relativeP2.x - (diff.x/diff.y * relativeP2.y);
+      if (x < 0 ) {
+        return null; // object is behind sensor
+      } else {
+        return {
+          distance: x,
+          cos: cos(diff)
+        }
+      }
+
+    }
 
   }
 }
