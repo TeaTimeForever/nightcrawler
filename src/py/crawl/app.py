9d3068c585c83ae0d783b179b0088649161e2834
Kriss Pujats <kpujats@evolutiongaming.com>
2017-09-14 10:45:57 +0300

    real deal

--- src/py/crawl/app.py
+++ src/py/crawl/app.py
@@ -3,11 +3,12 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
+from crawl.brain import find_wall_in_front
 from crawl import drive
 
 try:
-	# find_wall_in_front()
-	while True:
-		drive._left_forward()
+	find_wall_in_front()
+	# while True:
+	# 	drive.sharp_turn_left()
 except KeyboardInterrupt:
 	GPIO.cleanup()
