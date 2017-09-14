5130e041ecf2e0a58d082e9dec9d45691f269e0d
Kriss Pujats <kpujats@evolutiongaming.com>
2017-09-14 09:04:06 +0300

    do not hit the wall

--- src/crawl/app.py
+++ src/crawl/app.py
@@ -1,15 +1,11 @@
 import RPi.GPIO as GPIO
 
 from src.crawl.brain import find_wall_in_front
-from src.crawl.sonar import RotatingSonar
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
-ROTATING_SONAR = RotatingSonar(31, 32, 33)
-
 try:
 	find_wall_in_front()
 except KeyboardInterrupt:
-	ROTATING_SONAR.close()
 	GPIO.cleanup()
--- src/crawl/brain.py
+++ src/crawl/brain.py
@@ -1,14 +1,14 @@
 from src.crawl.drive import forward, stop
-from src.crawl.sonar import Sonar
+from src.crawl.sonar import Sonar, RotatingSonar
 from src.crawl.types import Distance
 
-_min_front_distance: Distance = 25
-_side_distance: Distance = 25
+_MIN_FRONT_DISTANCE: Distance = 25
+_MIN_SIDE_DISTANCE: Distance = 25
+ROTATING_SONAR = RotatingSonar(31, 32, 33)
 
 
-def find_wall_in_front(sonar: Sonar):
-	if sonar.distance() > _min_front_distance:
+def find_wall_in_front():
+	if ROTATING_SONAR.distance() > _MIN_FRONT_DISTANCE:
 		forward()
-		sonar.wait_until(lambda distance: distance < _min_front_distance)
+		ROTATING_SONAR.wait_until(dict([(90, lambda distance: distance < _MIN_FRONT_DISTANCE)]))
 		stop()
-
