f43401c35ecd898e27cce840373f1ebe2dbeeba8
kriss <kriss.pujats@gmail.com>
2017-09-16 00:37:33 +0300

    fix stop criteria

--- src/py/crawl/brain.py
+++ src/py/crawl/brain.py
@@ -8,7 +8,6 @@ ROTATING_SONAR = RotatingSonar(31, 32, 33)
 
 
 def find_wall_in_front():
-	if ROTATING_SONAR.distance() > _MIN_FRONT_DISTANCE:
-		forward()
-		ROTATING_SONAR.wait_until(dict([(90, lambda distance: distance < _MIN_FRONT_DISTANCE)]))
-		stop()
+	forward()
+	ROTATING_SONAR.wait_until(dict([(90, lambda distance: distance > _MIN_FRONT_DISTANCE)]))
+	stop()
