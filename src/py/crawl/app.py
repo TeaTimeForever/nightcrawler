f90c705341cc8a4d433af7349fd379801e4344ae
Kriss Pujats <kpujats@evolutiongaming.com>
2017-09-29 09:47:40 +0300

    check servo angle again

--- src/py/crawl/app.py
+++ src/py/crawl/app.py
@@ -1,8 +1,10 @@
 import RPi.GPIO as GPIO
 
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
+from crawl.sonar import ROTATING_SONAR
 from crawl.brain import crawl1, forward_backward
 from crawl import drive
 
@@ -11,8 +13,8 @@ from crawl import drive
 # except KeyboardInterrupt:
 # 	GPIO.cleanup()
 
-try:
-	drive.slow_down_before_wall()
-except KeyboardInterrupt:
-	drive.close()
-	GPIO.cleanup()
+# try:
+# 	drive.slow_down_before_wall()
+# except KeyboardInterrupt:
+# 	drive.close()
+# 	GPIO.cleanup()
